1356ade105919d3bb1354d72d8b7f4121af5547d
Nuha Syed <43392955+NobodyNamine@users.noreply.github.com>
2021-05-22 00:25:49 -0400

    main menu, timer, fail screen working

---
+++ OutofStock/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    static int numOfPS5 = 0;
+    static int levelNum = 0;
+
+    public Text Timer;
+    public bool PurchasedPS5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeRanOut();
+    }
+
+    public void TimeRanOut()
+    {
+        if ((int.Parse(Timer.text) <= 0) && PurchasedPS5 == false)
+        {
+            int randNum = Random.Range(1, 4);
+
+            increaseLevelNum();
+
+            //Show one random of 3 fail screens
+            switch (randNum)
+            { 
+                case 1:
+                    SceneManager.LoadScene("Fail1");
+                    break;
+                case 2:
+                    SceneManager.LoadScene("Fail2");
+                        break;
+                case 3:
+                    SceneManager.LoadScene("Fail3");
+                    break;
+            }
+            
+        }
+    }
+
+    public void increaseLevelNum()
+    {
+        levelNum++;
+    }
+
+}
+
---
+++ OutofStock/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    //Main Menu Logic
+    const int numOfLevels = 4; //Assuming Main Menu -> Game -> Result
+    public void PlayGame()
+    {
+        if (SceneManager.GetActiveScene().buildIndex <= (numOfLevels-1))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("Going to next scene");
+        }
+        else
+        {
+            //go back to main menu if no screens left
+            SceneManager.LoadScene(0);
+            Debug.Log("Going to Main Menu");
+
+        }
+
+
+
+    }
+
+    public void Again()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Going to Main Menu");
+    }
+
+    //quits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
